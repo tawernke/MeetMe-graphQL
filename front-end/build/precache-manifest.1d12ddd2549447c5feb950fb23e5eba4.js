938792208aa2154fcfb186ad88d4f3cdada69b8b
Tim <t.a.wernke@gmail.com>
2019-01-24 14:11:28 -0800

    Build done

---
+++ front-end/build/precache-manifest.1d12ddd2549447c5feb950fb23e5eba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05f7d994d0d2667f1550",
+    "url": "/static/js/main.05f7d994.chunk.js"
+  },
+  {
+    "revision": "19f09202b6236335e69f",
+    "url": "/static/js/1.19f09202.chunk.js"
+  },
+  {
+    "revision": "05f7d994d0d2667f1550",
+    "url": "/static/css/main.81c78944.chunk.css"
+  },
+  {
+    "revision": "19f09202b6236335e69f",
+    "url": "/static/css/1.232ecb2d.chunk.css"
+  },
+  {
+    "revision": "f6903666d2adfc2f1046bb9f1476c51a",
+    "url": "/index.html"
+  }
+];
