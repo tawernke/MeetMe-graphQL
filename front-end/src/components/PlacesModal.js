eb263ebcd254bf2d3810d2ed9a6152a3eeeb7eda
Tim <t.a.wernke@gmail.com>
2019-02-16 19:18:47 -0800

    Bug Fix: Places modal map populating with correct address

--- front-end/src/components/PlacesModal.js
+++ front-end/src/components/PlacesModal.js
@@ -8,7 +8,7 @@ class PlacesModal extends Component {
   }
 
   render() {
-    const {name, address_1, rating, phone, city, state, country} = this.props.place
+    const {name, address1, rating, phone, city, state, country} = this.props.place
     return(
 
         <Modal
@@ -19,10 +19,10 @@ class PlacesModal extends Component {
           height={500}
         >
           <div>
-            <iframe className= 'map-iframe' src={`https://www.google.com/maps/embed/v1/search?q=${address_1}&key=AIzaSyAW_Dcil3II5E7B5xjyY2W3S4Tmq7oPNis`} allowFullScreen title="Google Map"></iframe> 
+            <iframe className= 'map-iframe' src={`https://www.google.com/maps/embed/v1/search?q=${address1}&key=AIzaSyAW_Dcil3II5E7B5xjyY2W3S4Tmq7oPNis`} allowFullScreen title="Google Map"></iframe> 
           </div>
           <p>Phone: {phone}</p>
-          <p>Address: {address_1}, {city}, {state}, {country}</p>
+          <p>Address: {address1}, {city}, {state}, {country}</p>
           <p>Rating: {rating}</p>
         </Modal>
 
